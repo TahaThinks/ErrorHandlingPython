88ac3b6c6f699550044b96cbfd3920f851acf973
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-24 15:38:35 +0400

    Refine test.py

--- tests.py
+++ tests.py
@@ -1,24 +1,17 @@
 # FileNotFound Handling
 try:
     file = open("a_file.txt")
-    a_dictionary = {"key" : "value"}
-    print(a_dictionary["non_existent_key"])
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
 except FileNotFoundError:
     file = open("a_file.txt", 'w')
     file.write("Hello Taha!")
 except KeyError as error_message:
     print(f"The Key {error_message} doesn't Exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File is closed.")
 
-
-
-# # KeyError
-# a_dictionary = {"key": "value"}
-# value = a_dictionary["non_exisitent_key"]
-#
-# # IndexError
-# fruit_list = ["Apple", "Banana", "Pear"]
-# fruit = fruit_list[3]
-#
-# # TypeError
-# text = "abc"
-# print(text + 5)
