a703827bab38ce0be23cac82202d0ac0bcd9c755
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-24 15:31:27 +0400

    Write on the file

--- tests.py
+++ tests.py
@@ -3,15 +3,18 @@ try:
     file = open("a_file.txt")
 except:
     file = open("a_file.txt", 'w')
+    file.write("Hello Taha!")
 
-# KeyError
-a_dictionary = {"key": "value"}
-value = a_dictionary["non_exisitent_key"]
 
-# IndexError
-fruit_list = ["Apple", "Banana", "Pear"]
-fruit = fruit_list[3]
 
-# TypeError
-text = "abc"
-print(text + 5)
+# # KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_exisitent_key"]
+#
+# # IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+#
+# # TypeError
+# text = "abc"
+# print(text + 5)
