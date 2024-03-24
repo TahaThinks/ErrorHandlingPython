84e4db093b562c15d25a48954e64d128d283cfa4
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-24 15:43:25 +0400

    Raise an Error of BMI

--- tests.py
+++ tests.py
@@ -1,16 +1,8 @@
-# FileNotFound Handling
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", 'w')
-    file.write("Hello Taha!")
-except KeyError as error_message:
-    print(f"The Key {error_message} doesn't Exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    raise KeyError("This is an error created by Taha")
+height = float(input("Height: "))
+weight = float(input("Weight: "))
 
+if height > 3:
+    raise ValueError("Human Height is out of Range")
+
+bmi = weight / height ** 2
+print(bmi)
